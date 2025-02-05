4153d90c0bd8cf95eff23a301d0d0201bc87a8e7
Loulou Bvv <loulou@device-54.home>
2025-02-05 21:04:11 +0100

    premier commit - Ajout portfolio

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Exemple de fonction JavaScript pour ajouter des interactions
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Portfolio chargé !');
+  // Vous pouvez ajouter des animations ou des fonctionnalités ici
+});
